57a44363374b5d5d30d2fab9262d047be7a58584
Ilya <ilyawv@gmail.com>
2024-10-06 13:51:38 +0300

    fixed HCL with fmt command

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,30 +1,24 @@
 
 
-variable "vcn_name" {  
-    type = string
-    description = "name of the vcn"
-}    
+variable "vcn_name" {
+  type        = string
+  description = "name of the vcn"
+}
 
 
 variable "compartment_id" {
-    type = string
-    description = "id of the parent compartment"
+  type        = string
+  description = "id of the parent compartment"
 }
 
 variable "vcn_cidrs" {
-    type = list(string)
-    description = "cidr blocks of vnc"
+  type        = list(string)
+  description = "cidr blocks of vnc"
 }
 
-variable "subnets" {
-  description = "Private or Public subnets in a VCN"
-  type        = any
-  default     = {}
-  
-}
 
 variable "network_security_group_display_name" {
-    type = string
-    description = "name of the network security group"
-  
+  type        = string
+  description = "name of the network security group"
+
 }

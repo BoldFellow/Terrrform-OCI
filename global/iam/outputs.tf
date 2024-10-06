75860e6d89555fc1a00045437d5f24d2aa9825ee
Ilya <ilyawv@gmail.com>
2024-10-06 13:53:03 +0300

    fmted the IAM section as well

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -1,4 +1,4 @@
-output "israel_compartment_id" {   
-    value = oci_identity_compartment.test_compartment.id
-    description = "value of the Israel compartment id"
+output "israel_compartment_id" {
+  value       = oci_identity_compartment.test_compartment.id
+  description = "value of the Israel compartment id"
 }
--- global/iam/variables.tf
+++ global/iam/variables.tf
@@ -1,16 +1,16 @@
 variable "compartment_id" {
-    type = string
-    description = "id of the parent compartment"
-  
+  type        = string
+  description = "id of the parent compartment"
+
 }
 
 variable "compartment_description" {
-    type = string
-    description = "description of the compartment"
-  
+  type        = string
+  description = "description of the compartment"
+
 }
 
 variable "compartment_name" {
-    type = string
-    description = "Name of the compartment" 
+  type        = string
+  description = "Name of the compartment"
 }
